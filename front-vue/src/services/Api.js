4d9a4b1464b37fa404793434788419a4f2e9cd05
Vinicius Oliveira <67213864+vinicius-hso@users.noreply.github.com>
2021-06-14 20:21:58 -0300

    Delete front-vue directory

--- front-vue/src/services/Api.js
+++
@@ -1,8 +0,0 @@
-// Arquivo responsável por inicializar o axios e as requisições da url base dos HTTP's
-
-import axios from 'axios';
-
-export default () => axios.create({
-    // 'baseURL' do Back-End -> fará a comunicação do Back com o Front
-    baseURL: 'http://localhost:3000/api'
-});
